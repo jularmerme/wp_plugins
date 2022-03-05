b7e22ac26420e4127d3a2054260f55d7b0ecc28c
Julian Mercado <julianmercadomeza@gmail.com>
2022-03-05 02:25:32 -0500

    Add new plugin movie reviews

---
+++ movie-reviews/my_movie-reviews.php
@@ -0,0 +1,263 @@
+<?php
+
+/**
+ * Movie Reviews
+ *
+ * @package           PluginPackage
+ * @author            Julian Mercado
+ * @copyright         2022 Julian Mercado
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Movie Reviews
+ * Plugin URI:        https://example.com/plugin-name
+ * Description:       Add new custom post type to wordpress core.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Julian Mercado
+ * Author URI:        https://example.com
+ * Text Domain:       movie_reviews
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://example.com/my-plugin/
+ */
+
+
+
+require_once dirname( __FILE__ ) . '/lib/class-tgm-plugin-activation.php';
+
+class My_Movie_Reviews {
+	private static $instance;
+
+	const FIELD_PREFIX = 'jcmr_';
+	
+	// this needs to be hard-coded, but this serves as a reminder,
+	// and a find replace when searching and replacing
+	const TEXT_DOMAIN = 'jc-movie-reviews';
+
+  const CPT_SLUG = 'movie_review';
+
+	public static function getInstance() {
+		if (self::$instance == NULL) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	private function __construct() {
+		// initialize Movie Review custom post type
+		add_action('init', 'My_Movie_Reviews::register_post_type' );
+    // initialize Movie Type Taxonomies for Movie Reviews
+    add_action('init', 'My_Movie_Reviews::register_taxonomies');
+
+		// initialize custom fields from Metabox.io:
+		// first check for required plugin
+		add_action( 'tgmpa_register', array( $this, 'check_required_plugins' ) );
+		// then define the fields
+		add_filter( 'rwmb_meta_boxes', array( $this, 'metabox_custom_fields' ) );
+    // Add custom template and stylesheet for a single movie review page
+    add_action('template_include', array($this, 'add_cpt_template'));
+    add_action('wp_enqueue_scripts', array($this, 'add_styles_scripts'));
+	}
+
+	/**
+	 * Registers the Movie Review custom post type
+	 *
+	 * Defined statically for use in activation hook
+	 */
+	public static function register_post_type() {
+		register_post_type(self::CPT_SLUG, array(
+			'labels' => array(
+				'name' => __('Movie Reviews'),
+				'singular_name' => __('Movie Review'),
+			),
+			'description' => __('Highly opinionated movie reviews'),
+			'supports' => array(
+				'title', 'editor', 'excerpt', 'author', 'revisions', 'thumbnail',
+			),
+			'public' => TRUE,
+			'menu_icon' => 'dashicons-format-video',
+			'menu_position' => 4,
+		));
+	}
+
+  /***
+   * 
+   *  Register the Movie Review Taxonomies
+   * 
+   *  Defined statically for use in activation hook
+   */
+  public static function register_taxonomies() {
+    register_taxonomy( 'movie_types', array(self::CPT_SLUG), array(
+      'labels' => array(
+				'name' => __('Movie Types'),
+				'singular_name' => __('Movie Type'),
+			),
+			'public' => TRUE,
+      'hierarchical' => TRUE,
+      'rewrite' => array(
+        'slug' => 'movie-types',
+      )
+    ) );
+  }
+
+
+
+	/**
+	 * Activation hook (see register_activation_hook)
+	 */
+	public static function activate() {
+		self::register_post_type();
+    self::register_taxonomies();
+		flush_rewrite_rules();
+	}
+
+	/**
+	 * Implementation of the TGM Plugin Activation library
+	 *
+	 * Checks for the plugin(s) we need, and displays the appropriate messages
+	 */
+	function check_required_plugins() {
+		$plugins = array(
+			array(
+				'name'               => 'Meta Box',
+				'slug'               => 'meta-box',
+				'required'           => true,
+				'force_activation'   => false,
+				'force_deactivation' => false,
+			),
+		);
+
+		$config  = array(
+			'domain'           => 'jc_movie_reviews',
+			'default_path'     => '',
+			'parent_slug'      => 'plugins.php',
+			'capability'       => 'update_plugins',
+			'menu'             => 'install-required-plugins',
+			'has_notices'      => true,
+			'is_automatic'     => false,
+			'message'          => '',
+			'strings'          => array(
+				'page_title'                      => __( 'Install Required Plugins', 'jc-movie-reviews' ),
+				'menu_title'                      => __( 'Install Plugins', 'jc-movie-reviews' ),
+				'installing'                      => __( 'Installing Plugin: %s', 'jc-movie-reviews' ),
+				'oops'                            => __( 'Something went wrong with the plugin API.', 'jc-movie-reviews' ),
+				'notice_can_install_required'     => _n_noop( 'The Movie Reviews plugin depends on the following plugin: %1$s.', 'The Movie Reviews plugin depends on the following plugins: %1$s.' ),
+				'notice_can_install_recommended'  => _n_noop( 'The Movie Reviews plugin recommends the following plugin: %1$s.', 'The Movie Reviews plugin recommends the following plugins: %1$s.' ),
+				'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ),
+				'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ),
+				'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ),
+				'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ),
+				'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ),
+				'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ),
+				'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
+				'activate_link'                   => _n_noop( 'Activate installed plugin', 'Activate installed plugins' ),
+				'return'                          => __( 'Return to Required Plugins Installer', 'jc-movie-reviews' ),
+				'plugin_activated'                => __( 'Plugin activated successfully.', 'jc-movie-reviews' ),
+				'complete'                        => __( 'All plugins installed and activated successfully. %s', 'jc-movie-reviews' ),
+				'nag_type'                        => 'updated',
+			)
+		);
+		tgmpa( $plugins, $config );
+	}
+
+	/**
+	 * Create custom fields using metabox.io
+	 */
+	function metabox_custom_fields() {
+		// define the movie custom fields
+		$meta_boxes[] = array(
+			'id'       => 'movie_data',
+			'title'    => 'Additional Information',
+			'pages'    => array( self::CPT_SLUG ),
+			'context'  => 'normal',
+			'priority' => 'high',
+			'fields' => array(
+				array(
+					'name'  => 'Release Year',
+					'desc'  => 'Year the movie was released',
+					'id'    => self::FIELD_PREFIX . 'movie_year',
+					'type'  => 'number',
+					'std'   => date('Y'),
+					'min'   => '1896',
+				),
+				array(
+					'name'  => 'Director',
+					'desc'  => 'Who directed this movie',
+					'id'    => self::FIELD_PREFIX . 'movie_director',
+					'type'  => 'text',
+					'std'   => '',
+				),
+				array(
+					'name'  => 'IMDB Link',
+					'desc'  => 'Link for this movie on IMDB',
+					'id'    => self::FIELD_PREFIX . 'movie_imdb',
+					'type'  => 'url',
+					'std'   => '',
+				),
+			)
+		);
+
+		// define the review custom field(s)
+		$meta_boxes[] = array(
+			'id'       => 'review_data',
+			'title'    => 'Review',
+			'pages'    => array( self::CPT_SLUG ),
+			'context'  => 'side',
+			'priority' => 'high',
+			'fields' => array(
+				array(
+					'name'    => 'Rating',
+					'desc'    => 'On a scale of 1 - 10, 10 being best',
+					'id'      => self::FIELD_PREFIX . 'review_rating',
+					'type'    => 'select',
+					'options' => array(
+						'' => __('TBR (To be rated)'),
+						1  => __('1 - I walked out. And I was home!'),
+						2  => __('2 - I will never get those hours back'),
+						3  => __('3 - Not recommended'),
+						4  => __('4 - Might stay awake on an airplane for it'),
+						5  => __('5 - As they say on the internet: meh'),
+						6  => __('6 - Totally decent'),
+						7  => __('7 - Quite good. Recommended.'),
+						8  => __('8 - One of my favorites of the last X years'),
+						9  => __('9 - Loved it, and you probably will too.'),
+						10 => __("10 - Life changing! Mine, yours, everyone's!"),
+					),
+					'std' => '',
+				),
+			)
+		);
+
+		return $meta_boxes;
+	}
+
+  function add_cpt_template($template) {
+    if(is_singular(self::CPT_SLUG)) {
+      //check the active theme directory
+      if(file_exists(get_stylesheet_directory().'/single-'.self::CPT_SLUG.'.php')) {
+        return get_stylesheet_directory().'/single-'.self::CPT_SLUG.'.php';
+      }
+      //failing that, use the bundled copy
+      return plugin_dir_path( __FILE__ ).'templates/single-'.self::CPT_SLUG.'.php';
+    }
+    return $template;
+  } 
+
+  /**
+   * Enqueue the stylsheet for the movie review post type
+   */
+  function add_styles_scripts() {
+    wp_enqueue_style('movie-review-style', plugin_dir_url(__FILE__).'css/style.css');
+  }
+
+}
+
+// initialize plugin
+My_Movie_Reviews::getInstance();
+
+register_deactivation_hook( __FILE__, 'flush_rewrite_rules' );
+register_activation_hook( __FILE__, 'My_Movie_Reviews::activate' );
+
